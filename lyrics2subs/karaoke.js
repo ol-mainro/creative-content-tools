2f6b1e6ea13b286ceb389c697a4a6f73ee7f0a8e
ol-mainro <127300257+ol-mainro@users.noreply.github.com>
2023-03-11 09:02:28 +0100

    Update karaoke.js

--- lyrics2subs/karaoke.js
+++ lyrics2subs/karaoke.js
@@ -23,18 +23,25 @@ const queryString = window.location.search;
 
 const urlParams = new URLSearchParams(queryString);
 
-var bpm = 83;
-
 var text = urlParams.get('comment');
+var font = urlParams.get('font');
 var bpm = urlParams.get('tempo');
+var fontsize = urlParams.get('fontsize');
+
+if(!text) text = '\r\n\r\n1er vers\r\n2ème vers\r\n3ème vers\r\n\r\n4eme vers\r\n5ème vers\r\n6ème vers';
+if(!font) font = 'Geneva';
+if(!bpm) bpm = 80;
+if(!fontsize) fontsize = 10;
 
 document.getElementById('comment').value = text;
 document.getElementById('tempo').value = bpm;
+document.getElementById('font').value = font;
+document.getElementById('fontsize').value = fontsize;
 
 console.log(mySplitLines(text));
 var textarray = mySplitLines(text);
 
-var intro = '[Script Info]\r\n[V4+ Styles]\r\nFormat: Name, Fontname, Fontsize, PrimaryColour, SecondaryColour, OutlineColour, BackColour, Bold, Italic, Underline, StrikeOut, ScaleX, ScaleY, Spacing, Angle, BorderStyle, Outline, Shadow, Alignment, MarginL, MarginR, MarginV, Encoding\r\nStyle: Default,American Typewriter,90,&H00FFFFFF,&H000000FF,&H00000000,&H00000000,0,0,0,0,100,100,0,0,4,2,2,5,10,10,10,1\r\n\r\n[Events]\r\nFormat: Layer, Start, End, Style, Name, MarginL, MarginR, MarginV, Effect, Text'
+var intro = '[Script Info]\r\n[V4+ Styles]\r\nFormat: Name, Fontname, Fontsize, PrimaryColour, SecondaryColour, OutlineColour, BackColour, Bold, Italic, Underline, StrikeOut, ScaleX, ScaleY, Spacing, Angle, BorderStyle, Outline, Shadow, Alignment, MarginL, MarginR, MarginV, Encoding\r\nStyle: Default,' + font + ',' + fontsize + ',&H00FFFFFF,&H000000FF,&H00000000,&H00000000,0,0,0,0,100,100,0,0,4,2,2,5,10,10,10,1\r\n\r\n[Events]\r\nFormat: Layer, Start, End, Style, Name, MarginL, MarginR, MarginV, Effect, Text'
 
 console.log(intro);
 
